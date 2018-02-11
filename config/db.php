e4b9fb398283ba860be7bca316c2c5e88b4c0a9d
aminbr <aminbalavar93@gmail.com>
2018-02-11 03:57:32 +0330

    update
    update

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=gym_huva',
     'username' => 'root',
-    'password' => '',//Huva_97 
+    'password' => 'Huva_97',
     'charset' => 'utf8',
 ];
